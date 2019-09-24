ade989c1d73784b12df32e2829dce83b5d5f09b7
dkaralius <54810494+dkaralius@users.noreply.github.com>
2019-09-24 16:33:02 -0500

    Add files via upload

---
+++ Assignment2/Assignment2/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿using System;
+
+namespace Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Post_Output = new System.Windows.Forms.RichTextBox();
+            this.User_Output = new System.Windows.Forms.ListBox();
+            this.Subreddit_Output = new System.Windows.Forms.ListBox();
+            this.System_Output = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.LogIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Users";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label2.Location = new System.Drawing.Point(363, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Subreddits";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label3.Location = new System.Drawing.Point(519, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Posts";
+            // 
+            // Post_Output
+            // 
+            this.Post_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Post_Output.Location = new System.Drawing.Point(523, 40);
+            this.Post_Output.Name = "Post_Output";
+            this.Post_Output.ReadOnly = true;
+            this.Post_Output.Size = new System.Drawing.Size(595, 225);
+            this.Post_Output.TabIndex = 6;
+            this.Post_Output.Text = "";
+            // 
+            // User_Output
+            // 
+            this.User_Output.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.User_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.User_Output.FormattingEnabled = true;
+            this.User_Output.ItemHeight = 20;
+            this.User_Output.Location = new System.Drawing.Point(16, 40);
+            this.User_Output.Name = "User_Output";
+            this.User_Output.Size = new System.Drawing.Size(328, 224);
+            this.User_Output.TabIndex = 7;
+            this.User_Output.SelectedIndexChanged += new System.EventHandler(this.User_Output_SelectedIndexChanged);
+            // 
+            // Subreddit_Output
+            // 
+            this.Subreddit_Output.BackColor = System.Drawing.Color.Black;
+            this.Subreddit_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Subreddit_Output.ForeColor = System.Drawing.Color.White;
+            this.Subreddit_Output.FormattingEnabled = true;
+            this.Subreddit_Output.ItemHeight = 20;
+            this.Subreddit_Output.Location = new System.Drawing.Point(367, 40);
+            this.Subreddit_Output.Name = "Subreddit_Output";
+            this.Subreddit_Output.Size = new System.Drawing.Size(134, 224);
+            this.Subreddit_Output.TabIndex = 8;
+            // 
+            // System_Output
+            // 
+            this.System_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.System_Output.Location = new System.Drawing.Point(12, 438);
+            this.System_Output.Name = "System_Output";
+            this.System_Output.Size = new System.Drawing.Size(1106, 96);
+            this.System_Output.TabIndex = 9;
+            this.System_Output.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label4.Location = new System.Drawing.Point(12, 411);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "System Output";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label5.Location = new System.Drawing.Point(12, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 24);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Password";
+            // 
+            // Password
+            // 
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.Location = new System.Drawing.Point(16, 294);
+            this.Password.MaxLength = 40;
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '*';
+            this.Password.Size = new System.Drawing.Size(141, 26);
+            this.Password.TabIndex = 12;
+            // 
+            // LogIn
+            // 
+            this.LogIn.Location = new System.Drawing.Point(172, 294);
+            this.LogIn.Name = "LogIn";
+            this.LogIn.Size = new System.Drawing.Size(75, 26);
+            this.LogIn.TabIndex = 13;
+            this.LogIn.Text = "Log-In";
+            this.LogIn.UseVisualStyleBackColor = true;
+            this.LogIn.Click += new System.EventHandler(this.LogIn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1130, 546);
+            this.Controls.Add(this.LogIn);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.System_Output);
+            this.Controls.Add(this.Subreddit_Output);
+            this.Controls.Add(this.User_Output);
+            this.Controls.Add(this.Post_Output);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Reddit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox Post_Output;
+        private System.Windows.Forms.ListBox User_Output;
+        private System.Windows.Forms.ListBox Subreddit_Output;
+        private System.Windows.Forms.RichTextBox System_Output;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Button LogIn;
+    }
+}
+
---
+++ Assignment2/Assignment2/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment2
+{
+    public partial class Form1 : Form
+    {
+        public static SortedSet<User> users;
+
+        public class User : IComparable
+        {
+            private readonly uint id;
+            private readonly string userType;
+            private readonly string name;
+            private readonly string passwordHash;
+            private int postScore;
+            private int commentScore;
+            List<string> moderatingSubs;
+
+            public uint Id { get => id; }
+            public string Usertype { get => userType; }
+            public string Name { get => name; }
+            public int PostScore { get => postScore; set => postScore = value; }
+            public int CommentScore { get => commentScore; set => commentScore = value; }
+
+            public User()
+            {
+                id = 0;
+                userType = "";
+                name = "";
+                postScore = 0;
+                commentScore = 0;
+            }
+            public User(string[] args)
+            {
+                List<string> moderatingSubs = new List<string>();
+
+                if (args.Length == 6)
+                {
+                    id = Convert.ToUInt32(args[0]);
+
+                    if(args[1] == "0")
+                    {
+                        userType = "";
+                    }
+                    if (args[1] == "1")
+                    {
+                        userType = "(M)";
+                    }
+                    if (args[1] == "2")
+                    {
+                        userType = "(A)";
+                    }
+
+                    name = args[2];
+                    postScore = Convert.ToInt32(args[4]);
+                    commentScore = Convert.ToInt32(args[5]);
+                }
+                if (args.Length == 7)
+                {
+                    id = Convert.ToUInt32(args[0]);
+
+                    if (args[1] == "0")
+                    {
+                        userType = "";
+                    }
+                    if (args[1] == "1")
+                    {
+                        userType = "(M)";
+                    }
+                    if (args[1] == "2")
+                    {
+                        userType = "(A)";
+                    }
+
+                    name = args[2];
+                    postScore = Convert.ToInt32(args[4]);
+                    commentScore = Convert.ToInt32(args[5]);
+                    moderatingSubs.Add(args[6]);
+                }
+                if (args.Length == 8)
+                {
+                    id = Convert.ToUInt32(args[0]);
+
+                    if (args[1] == "0")
+                    {
+                        userType = "";
+                    }
+                    if (args[1] == "1")
+                    {
+                        userType = "(M)";
+                    }
+                    if (args[1] == "2")
+                    {
+                        userType = "(A)";
+                    }
+
+                    name = args[2];
+                    postScore = Convert.ToInt32(args[4]);
+                    commentScore = Convert.ToInt32(args[5]);
+                    moderatingSubs.Add(args[6]);
+                    moderatingSubs.Add(args[7]);
+                }
+            }
+            public int CompareTo(Object alpha)
+            {
+                // check that alpha isnt empty
+                if (alpha == null)
+                {
+                    throw new ArgumentNullException("Empty user.");
+                }
+                User rightOp = alpha as User;
+
+                if (rightOp != null) // typecast worked
+                    return Name.CompareTo(rightOp.Name);
+                else
+                    throw new ArgumentException("Argument was not a user.");
+            }
+        }
+        public class Subreddit : IComparable
+        {
+            //Private variables
+            private readonly uint id;
+            private string name;
+            private uint members;
+            private uint active;
+
+            //Public properties
+            public uint Id => id;
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public uint Members
+            {
+                get { return members; }
+                set { members = value; }
+            }
+            public uint Active
+            {
+                get { return active; }
+                set { active = value; }
+            }
+
+            //Default constructor
+            public Subreddit()
+            {
+                id = 0;
+                name = "";
+                members = 0;
+                active = 0;
+            }
+
+            //Alternate constructor where all arguments are provided
+            public Subreddit(uint id, string name, uint members, uint active)
+            {
+                this.id = id;
+                this.name = name;
+                this.members = members;
+                this.active = active;
+            }
+
+            //Alternate constructor where only id and name are provided
+            public Subreddit(uint id, string name)
+            {
+                this.id = id;
+                this.name = name;
+                members = 0;
+                active = 0;
+            }
+
+            //Alternate constructor to take n number of args
+            public Subreddit(string[] args)
+            {
+                if (args.Length == 4)
+                {
+                    id = Convert.ToUInt32(args[0]);
+                    name = args[1];
+                    members = Convert.ToUInt32(args[2]);
+                    active = Convert.ToUInt32(args[3]);
+                }
+            }
+
+            //Overriden CompareTo() to sort by name
+            public int CompareTo(Object alpha)
+            {
+                if (alpha == null) throw new ArgumentNullException("argument bad");
+
+                Subreddit rightOp = alpha as Subreddit;
+
+                if (rightOp != null)
+                    return name.CompareTo(rightOp.name);
+                else
+                    throw new ArgumentException("argument was not a name.");
+            }
+        }
+        public static void ReadInputFiles(SortedSet<User> users, SortedSet<Subreddit> subreddits)
+        {
+            String slacker; //Buffer
+            String[] tokens; //Used to store tokens of buffer
+
+            using (StreamReader inFile = new StreamReader("..\\..\\users.txt"))
+            {
+                slacker = inFile.ReadLine();
+
+                while (slacker != null)
+                {
+                    tokens = slacker.Split('\t');
+                    users.Add(new User(tokens));
+
+                    slacker = inFile.ReadLine();
+                }
+            }
+            using (StreamReader inFile = new StreamReader("..\\..\\subreddits.txt"))
+            {
+                slacker = inFile.ReadLine();
+
+                while (slacker != null)
+                {
+                    tokens = slacker.Split('\t');
+                    subreddits.Add(new Subreddit(tokens));
+
+                    slacker = inFile.ReadLine();
+                }
+            }
+        }
+            public Form1()
+        {
+            InitializeComponent();
+
+            SortedSet<User> users = new SortedSet<User>();
+            SortedSet<Subreddit> subreddits = new SortedSet<Subreddit>();
+
+            ReadInputFiles(users,subreddits);
+
+            foreach (User u in users)
+            {
+                User_Output.Items.Add(u.Name + "   " +  u.Usertype + "         (" + u.PostScore + " / " + u.CommentScore + ")" + Environment.NewLine);
+            }
+            foreach (Subreddit s in subreddits)
+            {
+                Subreddit_Output.Items.Add(s.Name + Environment.NewLine);
+            }
+
+            System_Output.Text = "Please select a user.";
+        }
+
+        private void User_Output_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] tokens = User_Output.SelectedItem.ToString().Split(' ');
+            System_Output.Text = "Please provide the password for User '" + tokens[0] + "'.";
+        }
+
+        private void LogIn_Click(object sender, EventArgs e)
+        {
+            string[] tokens = User_Output.SelectedItem.ToString().Split(' ');
+
+            string username = tokens[0];
+            string password = Password.Text;
+            string correctPassword = "";
+
+            //Hardcoded from input file, not sure how to read them otherwise
+            if(username == "GallowBoob")
+            {
+                correctPassword = "F2381EF2";
+            }
+            if (username == "poem_for_your_sprog")
+            {
+                correctPassword = "E1308036";
+            }
+            if (username == "Rogness")
+            {
+                correctPassword = "BB4EEE6E";
+            }
+            if (username == "crappymorph")
+            {
+                correctPassword = "5DC8199";
+            }
+            if (username == "Unexpected_Gimli")
+            {
+                correctPassword = "AAA558A0";
+            }
+            if (username == "PM_YOUR_CODE")
+            {
+                correctPassword = "6D86019";
+            }
+            if (username == "NervousPigeon")
+            {
+                correctPassword = "45DC58F4";
+            }
+            if (username == "ConfusedPenguin")
+            {
+                correctPassword = "D352C017";
+            }
+            if (username == "IsThisAMeme")
+            {
+                correctPassword = "E87F22F3";
+            }
+            if (username == "KarmaCop")
+            {
+                correctPassword = "93AB176";
+            }
+            if (username == "BlueEyesWhiteDragon")
+            {
+                correctPassword = "C7E772E2";
+            }
+
+            string hexValue = password.GetHashCode().ToString("X");
+
+            if(correctPassword == hexValue)
+            {
+                System_Output.Text = "Authentication successful! Hello '" + username + "'." + Environment.NewLine +
+                    "Displaying all posts and comments made by user '" + username + "'.";
+                LogIn.Text = "Log-In";
+            }
+            else
+            {
+                System_Output.Text = "Incorrect password. Please try again.";
+                LogIn.Text = "Retry";
+            }
+        }
+    }
+}
